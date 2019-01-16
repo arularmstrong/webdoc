33c5f2a809f3f9e9c7cef04e3981e2b2c8e55e9a
s.arularmstrong <s.arularmstrong@gmail.com>
2019-01-16 07:36:17 -0800

    i

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+
+    return """
+    <h1>Hello heroku</h1>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
